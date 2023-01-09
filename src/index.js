44ec476ab10ada54e24dbdaa591a43e152be4aa3
Oliv3r1 <oliver.suomela@metropolia.fi>
2023-01-09 15:30:33 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 //author: Oliver
 
-console.log("My first Webpack app!");
+console.log("Eka webpack app!");
+
+const a = 6;
+let b = 3;
+{
+  const a = 9;
+  const c = 4;
+  {
+    let c = 1;
+    console.log(a + b + c); // --> 13
+    b = 2;
+  }
+  console.log(a + b); // --> 11
+}
+console.log(a + b); // --> 8
+
+const powerIterative = (base, exponent) => {
+  let result = 1;
+  for (expo = 1; expo <= exponent; expo++) {
+    result *= base;
+  }
+  return result;
+};
+console.log(powerIterative(2, 2));
