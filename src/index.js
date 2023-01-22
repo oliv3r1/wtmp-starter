43c19ccb95bdf603ac12761d3e5e71284d8491c4
oliv3r1 <oliver.suomela@metropolia.fi>
2023-01-22 18:58:35 +0200

    task2

--- src/index.js
+++ src/index.js
@@ -1,49 +1,147 @@
-import LunchMenu from '../sodexo-day-example.json';
-// Test
-console.log('lunch menu object', LunchMenu);
+//author: Oliver
 
-let language = "en";
+const minNumber = 1;
+const maxNumber = 100;
+const maxGuesses = 10;
 
-const courses = document.getElementById("courses");
-let coursesList = [];
+let startTime;
+let endTime;
+let totalGuesses;
 
-for (let course in LunchMenu.courses) {
-    if (language === "en") {
-        coursesList.push(LunchMenu.courses[course].title_en);
-    } else {
-        coursesList.push(LunchMenu.courses[course].title_fi);
+let randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+let computerGuess = -1;
+let guessHistory = [];
+
+
+
+
+
+const checkGuess = () => {
+  const userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+    startTime = new Date();
+  }
+  guesses.textContent += `${userGuess} `;
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    playerGuesses = guesses.textContent;
+    endTime = new Date();
+    setGameOver();
+  } else if (guessCount === maxGuesses) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    lowOrHi.textContent = '';
+    endTime = new Date();
+    totalGuesses = guessCount;
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
     }
-}
-courses.innerHTML = coursesList.join("<br>");
+  }
 
-const LanguageBtn = document.getElementById("Language");
-LanguageBtn.addEventListener("click", function () {
-    if (language === "en") {
-        language = "fi";
-    } else {
-        language = "en";
-    }
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+};
 
-    coursesList = [];
-    for (let course in LunchMenu.courses) {
-        if (language === "en") {
-            coursesList.push(LunchMenu.courses[course].title_en);
+guessSubmit.addEventListener('click', function(){
+  checkGuess();
+  computerPlayer();
+});
+
+const setGameOver= () => {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+  const elapsedTime = (endTime - startTime) / 1000;
+    const elapsedTimeParagraph = document.createElement('p');
+    elapsedTimeParagraph.textContent = `Time Used: ${elapsedTime} seconds`;
+    document.querySelector('.resultParas').appendChild(elapsedTimeParagraph);
+  
+  if(lastResult.textContent === 'Congratulations! You got it right!') {
+    const guessesParagraph = document.createElement('p');
+    guessesParagraph.textContent = `Your guesses: ${guessCount}`;
+    document.querySelector('.resultParas').appendChild(guessesParagraph);
+  }
+};
+
+const resetGame =() => {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (const resetPara of resetParas) {
+    resetPara.textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+};
+
+
+
+const computerPlayer = () => {
+    //range of possible numbers
+    let min = minNumber;
+    let max = maxNumber;
+
+    //number of guesses
+    let guessCount = 0;
+
+     guessCount = 0;
+    guessHistory = [];
+    randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+    
+
+    // while the guess is not correct
+    while (computerGuess !== randomNumber) {
+        // increment the number of guesses
+        guessCount++;
+        computerGuess = Math.floor(Math.random() * (max - min + 1)) + min;
+        // save the guess to the history
+        guessHistory.push(computerGuess);
+        if (computerGuess === randomNumber) {
+            console.log(`It took the computer ${guessCount} guesses.`);
+            console.log(`Computer guess history: ${guessHistory}`);
+            break;
+        } else if (computerGuess < randomNumber) {
+            min = computerGuess + 1;
         } else {
-            coursesList.push(LunchMenu.courses[course].title_fi);
+            max = computerGuess - 1;
         }
     }
-    courses.innerHTML = coursesList.join("<br>");
-});
+    
+};
 
-const sortCoursesBtn = document.getElementById("sortCourses");
-sortCoursesBtn.addEventListener("click", function () {
-    coursesList.sort();
-    courses.innerHTML = coursesList.join("<br>");
-});
 
-const randomBtn = document.getElementById("randomBtn");
-randomBtn.addEventListener("click", function () {
-    const randomIndex = Math.floor(Math.random() * coursesList.length);
-    const randomDish = coursesList[randomIndex];
-    courses.innerHTML = randomDish;
-});
+
+
